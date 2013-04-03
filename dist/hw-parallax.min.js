0fdd190f9b857b068a1079a5521d313bd912f1e8
Ziad Saab <ziad.saab@gmail.com>
2013-04-02 22:07:56 -0400

    added e-mail address in license file

--- dist/hw-parallax.min.js
+++ dist/hw-parallax.min.js
@@ -1,5 +1,5 @@
 /*
- Copyright (C) 2013 Ziad Saab
+ Copyright (C) 2013 Ziad Saab ziad(dot)saab(at)gmail(dot)com
 
  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
  documentation files (the "Software"), to deal in the Software without restriction, including without limitation
